62acbf27de4b030221838bd28a933fa0c6a116e2
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2022-08-08 15:15:04 +0900

    4.4.0.0-TESTING7

---
+++ AdopPrebidSdk.xcframework/ios-arm64_x86_64-simulator/AdopPrebidSdk.framework/Headers/AdopPrebidReward.h
@@ -0,0 +1,38 @@
+//
+//  AdopPrebidReward.h
+//  AdopPrebidSdk
+//
+//  Created by Seungsub Oh on 2022/08/02.
+//
+
+@import Foundation;
+@import UIKit;
+@import PrebidMobile;
+#import "AdopPrebid.h"
+
+@protocol AdopPrebidRewardDelegate
+
+@optional
+- (void)onRewardLoad;
+- (void)onRewardShow;
+- (void)onRewardClick;
+- (void)onRewardClose;
+- (void)onRewardComplete;
+- (void)onRewardSkip;
+- (void)onRewardFail:(NSError * _Nonnull)error;
+
+@end
+
+@interface AdopPrebidReward : NSObject<RewardedAdUnitDelegate>
+
+@property (nonatomic, strong) id<AdopPrebidRewardDelegate> _Nullable delegate;
+@property (nonatomic) NSString * _Nonnull configId;
+@property (nonatomic) NSString * _Nullable storedAuctionResponse;
+
+- (id _Nullable)initWithParentViewController:(UIViewController * _Nonnull)parentVC;
+
+- (void)requestAd;
+- (BOOL)isLoaded;
+- (void)showAd;
+
+@end
